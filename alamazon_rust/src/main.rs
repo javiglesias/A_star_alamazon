4e434e69f1732d1076d0cb2bec77a85cd3489ae6
javiglesias <javiglesias@outlook.com>
2017-11-29 10:58:55 +0100

    starting to write some code writing the products input.

--- alamazon_rust/src/main.rs
+++ alamazon_rust/src/main.rs
@@ -1,41 +1,32 @@
 use std::io;
 mod lib;
 fn main() {
-    let mut option = String::new();
-    println!("MENU");
-    println!("1-Backtrack.");
-    println!("2-primeroProfundidad");
-    println!("3-primeroAmplitud");
-    println!("4-A*");
-    println!("5-MinMax");
-    println!("6-Alfabeta");
-    io::stdin().read_line(&mut option);
-    match 4 {
-        1 => {;}
-        4 => {a_star(0);}
-        _ => {;}
+    let mut objects: [String; 3] = Default::default();
+    let mut quatity: [String;3] = Default::default();
+    let mut c=0;
+    loop {
+        if(c == 3) {
+            break;
+        }else {
+            println!("Introduce object {} name: ",c);
+            io::stdin().read_line(&mut objects[c]);
+             println!("Introduce object {} quatity: ",c);
+            io::stdin().read_line(&mut quatity[c]);
+            c = c+1;
+        }
     }
-    fn a_star(mut n:i32) {
+    fn a_star(mut o:[String; 3], mut n:[String;3]) {
+        let mut n =0;
         println!("calling a_star: {}", n);
         let node_1 = lib::node{g:0, h:10};
         let node_2 = lib::node{g:1, h:10};
-       // loop {
             if n==2 { //condicion de salida de recursion
                 println!("f1: {}",node_1.f());
                 return;
             }
             else {
                     n = n+1;
-                    a_star(n); //recursividad tocha
+                    //a_star(n); //recursividad tocha
             }
-        //}
     }
-     io::stdin().read_line(&mut option);
 }
-//backtracking
-//primeroProfundidad
-//primeroAmplitud
-//A*
-//Minmax
-//Alfabeta
-//Refutación por resolucion
